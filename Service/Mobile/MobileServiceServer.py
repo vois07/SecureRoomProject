8747e7ff7be4d453312343ea34658e3d312823d2
eaglx <tango_delta@outlook.com>
2018-12-28 11:59:42 +0100

    Fix and add RPi3 server

--- Service/Mobile/MobileServiceServer.py
+++ Service/Mobile/MobileServiceServer.py
@@ -74,78 +74,78 @@ def recv_until(sock, marker, rcvlimit):
 
 class SrvHandler(threading.Thread):
     def __init__(self, clientSock, clientAddr):
-        super(Handler, self).__init__()
+        super(SrvHandler, self).__init__()
         self.clientSock = clientSock
         self.clientAddr = clientAddr
 
-  def run(self):
-    recvData = recv_until(self.clientSock, "\r\n\r\n", 102400) #102400 == 100KB
-    if(recvData != False):
-        usrid, scode, rdata = recvData.split("$$$")
-        test_code_value = False
-        try:
-          code = int(scode)
-        except:
-          print("[ WARNING ] Cannot get msg code!")
-          logging.warning('Cannot get msg code!')
-          test_code_value = True
-        if(test_code_value == False):
-         # Client download public key from server
-          if(code == 101):
-            messg = "101$$$" + str_pubKey
-            self.clientSock.sendall(messg.encode("utf-8"))
-            print("[ INFO ] Sent public key to ", usrid)
-            logging.info('Sent public key to ' + str(usrid))
-        # Send android’s user server's public key
-          elif(code == 102):
+    def run(self):
+        recvData = recv_until(self.clientSock, "\r\n\r\n", 102400) #102400 == 100KB
+        if(recvData != False):
+            usrid, scode, rdata = recvData.split("$$$")
+            test_code_value = False
             try:
-              rrdata = rdata.replace('\r\n\r\n', '')
-              f = open('publ_client_keys.dd', 'a')
-              rsa_android = base64.b64decode(rrdata)
-              publicKeyAndroid = RSA.importKey(rsa_android).publickey()
-              str_publicKeyAndroid = publicKeyAndroid.exportKey('PEM').decode('utf-8')
-              str_publicKeyAndroid = str_publicKeyAndroid.replace('-----BEGIN PUBLIC KEY-----', '')
-              str_publicKeyAndroid = str_publicKeyAndroid.replace('-----END PUBLIC KEY-----', '')
-              str_publicKeyAndroid = str_publicKeyAndroid.replace('\n', '')
-              #print("[ INFO ] ", usrid, " public key: ", str_publicKeyAndroid)
-              f.write("%s %s\n" % (usrid, str_publicKeyAndroid))
-              f.close()
-              clientRSA_PublicKeys[usrid] = publicKeyAndroid
+              code = int(scode)
             except:
-              print("[ WARNING ] Exception on code 102")
-              logging.warning('Exception on code 102')
-          # Ask to enter to the room
-          #elif(code == 103):
-          # Ask to exit the room
-          #elif(code == 104):
-          # Send data to Android's user
-          #elif(code == 105):
-          #
-          elif(code == 201):
-            print("[ INFO ] TEST CONNECTION WITH ", usrid, " RECIVE: ", rdata.replace('\r\n\r\n', ''))
-            messg = "SERV_ANSWER".encode("utf-8")
-            self.clientSock.sendall(messg)
-          elif(code == 202):
-            try:
-              rrdata = rdata.replace('\r\n\r\n', '')
-              dec_data = myKey.decrypt(base64.b64decode(rrdata))
-              print("[ INFO ] Recived data: ", dec_data.decode('utf-8'), " from: ", usrid)
-              messg = "SERVER_HELLO_WORLD".encode('utf-8')
-              enc_messg = clientRSA_PublicKeys[usrid].encrypt(messg, 32)
-              sendData = "DATA:" + str(base64.b64encode(enc_messg[0]))
-              self.clientSock.sendall(sendData.encode("utf-8"))
-            except:
-              print("[ WARNING ] Exception on code 202")
-              logging.warning('Exception on code 202')
-          else:
-            print("[ WARNING ] Unknow code: ", code)
-            logging.warning('Unknow code: ' + str(code))
-    #Close connection in both sides: client and server, no data send
-    self.clientSock.shutdown(socket.SHUT_RDWR)
-    self.clientSock.close()
+              print("[ WARNING ] Cannot get msg code!")
+              logging.warning('Cannot get msg code!')
+              test_code_value = True
+            if(test_code_value == False):
+             # Client download public key from server
+              if(code == 101):
+                messg = "101$$$" + str_pubKey
+                self.clientSock.sendall(messg.encode("utf-8"))
+                print("[ INFO ] Sent public key to ", usrid)
+                logging.info('Sent public key to ' + str(usrid))
+            # Send android’s user server's public key
+              elif(code == 102):
+                try:
+                  rrdata = rdata.replace('\r\n\r\n', '')
+                  f = open('publ_client_keys.dd', 'a')
+                  rsa_android = base64.b64decode(rrdata)
+                  publicKeyAndroid = RSA.importKey(rsa_android).publickey()
+                  str_publicKeyAndroid = publicKeyAndroid.exportKey('PEM').decode('utf-8')
+                  str_publicKeyAndroid = str_publicKeyAndroid.replace('-----BEGIN PUBLIC KEY-----', '')
+                  str_publicKeyAndroid = str_publicKeyAndroid.replace('-----END PUBLIC KEY-----', '')
+                  str_publicKeyAndroid = str_publicKeyAndroid.replace('\n', '')
+                  #print("[ INFO ] ", usrid, " public key: ", str_publicKeyAndroid)
+                  f.write("%s %s\n" % (usrid, str_publicKeyAndroid))
+                  f.close()
+                  clientRSA_PublicKeys[usrid] = publicKeyAndroid
+                except:
+                  print("[ WARNING ] Exception on code 102")
+                  logging.warning('Exception on code 102')
+              # Ask to enter to the room
+              #elif(code == 103):
+              # Ask to exit the room
+              #elif(code == 104):
+              # Send data to Android's user
+              #elif(code == 105):
+              #
+              elif(code == 201):
+                print("[ INFO ] TEST CONNECTION WITH ", usrid, " RECIVE: ", rdata.replace('\r\n\r\n', ''))
+                messg = "SERV_ANSWER".encode("utf-8")
+                self.clientSock.sendall(messg)
+              elif(code == 202):
+                try:
+                  rrdata = rdata.replace('\r\n\r\n', '')
+                  dec_data = myKey.decrypt(base64.b64decode(rrdata))
+                  print("[ INFO ] Recived data: ", dec_data.decode('utf-8'), " from: ", usrid)
+                  messg = "SERVER_HELLO_WORLD".encode('utf-8')
+                  enc_messg = clientRSA_PublicKeys[usrid].encrypt(messg, 32)
+                  sendData = "DATA:" + str(base64.b64encode(enc_messg[0]))
+                  self.clientSock.sendall(sendData.encode("utf-8"))
+                except:
+                  print("[ WARNING ] Exception on code 202")
+                  logging.warning('Exception on code 202')
+              else:
+                print("[ WARNING ] Unknow code: ", code)
+                logging.warning('Unknow code: ' + str(code))
+        #Close connection in both sides: client and server, no data send
+        self.clientSock.shutdown(socket.SHUT_RDWR)
+        self.clientSock.close()
 
 def main():
-  print("[ INFO ] APS v0.4")
+  print("[ INFO ] MobileServiceServer v0.4")
   #Get server IP
   ip_server = ni.ifaddresses(NET_DEVICE_INTERFACE)[ni.AF_INET][0]['addr']
   print("[ INFO ] IP: ",ip_server," PORT: ",SERVER_PORT, " DEV: ", NET_DEVICE_INTERFACE)
---
+++ Service/RPi3/RpiServiceServer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import socket
+import subprocess
+import sys
+import os.path
+import threading
+import telnetlib
+import netifaces as ni
+import base64
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import logging
+import time
+
+MAX_CONNECTIONS_IN_QUEUE = 5
+SERVER_PORT = 7133
+NET_DEVICE_INTERFACE = 'enp0s8'
+
+def recv_until(sock, marker, rcvlimit):
+  data = ""
+  while(data.find(marker) == -1):
+    try:
+      dnow = sock.recv(1)
+      dnow = dnow.decode('utf-8')
+      if(len(dnow) == 0):
+        print("[ WARNING ] recv_until function failed at recv. Last recived data:", data)
+        return False
+    except socket.error as msg:
+      print("[ WARNING ] recv_until function failed: ", msg, " Last recived data:", data)
+      return False
+    data += dnow
+    if len(data) > rcvlimit:
+      print("[ WARNING ] recv_until function failed: DATA_LIMIT_REACHED: ", rcvlimit," Last recived data:", data)
+      return False
+  return data
+
+class SrvHandler(threading.Thread):
+    def __init__(self, clientSock, clientAddr):
+        super(SrvHandler, self).__init__()
+        self.clientSock = clientSock
+        self.clientAddr = clientAddr
+
+    def run(self):
+        recvData = recv_until(self.clientSock, "\r\n\r\n", 102400) #102400 == 100KB
+        if(recvData != False):
+            rdata = recvData
+            print(rdata)
+        #Close connection in both sides: client and server, no data send
+        self.clientSock.shutdown(socket.SHUT_RDWR)
+        self.clientSock.close()
+
+
+def main():
+  print("[ INFO ] RpiServiceServer v0.1")
+  #Get server IP
+  ip_server = ni.ifaddresses(NET_DEVICE_INTERFACE)[ni.AF_INET][0]['addr']
+  print("[ INFO ] IP: ",ip_server," PORT: ",SERVER_PORT, " DEV: ", NET_DEVICE_INTERFACE)
+  #TCP
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server.bind((ip_server, SERVER_PORT))
+  #Max connection in queue
+  server.listen(MAX_CONNECTIONS_IN_QUEUE)
+
+  try:
+    while True:
+      clientSock, clientAddr = server.accept()
+      clientThread = SrvHandler(clientSock, clientAddr)
+      clientThread.daemon = False #When kill main process, then it kill threads
+      clientThread.start()
+  except KeyboardInterrupt:
+    print("[ Keyboard Interrupt ] Close server\n")
+    server.close()
+
+if __name__ == "__main__":
+  main()
