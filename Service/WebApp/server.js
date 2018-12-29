bce7ed1416f664328a6f7f014541633732eb6123
jeedrzej <jeedrzej11@gmail.com>
2018-12-29 19:44:05 +0100

    Webapp: Dane na stronie - wersja podstawowa

--- Service/WebApp/server.js
+++ Service/WebApp/server.js
@@ -51,7 +51,62 @@ var con = mysql.createConnection({
   });
   */
 });
-
+function newDate(x){
+	var res = x.split(':');
+	res.push(res[0].split(" "));
+	var res2 = [];
+	res2.push(res[3][2]);
+	res2.push(res[3][0]);
+	res2.push(res[3][1]);
+	res2.push(res[3][3]);
+	res2.push(res[1]);
+	res2.push(res[2]);
+	res2[0]=parseInt(res2[0]);
+	res2[2]=parseInt(res2[2]);
+	res2[3]=parseInt(res2[3]);
+	res2[4]=parseInt(res2[4]);
+	res2[5]=parseInt(res2[5]);
+	switch(res2[1]){
+		case "Jan":
+		res2[1]=1;
+		break;
+		case "Feb":
+		res2[1]=2;
+		break;
+		case "Mar":
+		res2[1]=3;
+		break;
+		case "Apr":
+		res2[1]=4;
+		break;
+		case "May":
+		res2[1]=5;
+		break;
+		case "Jun":
+		res2[1]=6;
+		break;
+		case "Jul":
+		res2[1]=7;
+		break;
+		case "Aug":
+		res2[1]=8;
+		break;
+		case "Sep":
+		res2[1]=9;
+		break;
+		case "Oct":
+		res2[1]=10;
+		break;
+		case "Nov":
+		res2[1]=11;
+		break;
+		case "Dec":
+		res2[1]=12;
+		break;
+	}
+	var date = new Date(res2[0],res2[1],res2[2],res2[3]+1,res2[4],res2[5])
+	return date;
+}
 
 var eventEmitter = new events.EventEmitter();
 //Główna pętla serwera
@@ -61,6 +116,8 @@ var labelsResult;
 var temperature1;
 var temperature2;
 var temperature3;
+var people;
+var data_time;
 	//Wgrywanie strony głównej
 	
 con.query("select * from measurements order by measur_date ASC", function (err, result) {
@@ -70,19 +127,34 @@ con.query("select * from measurements order by measur_date ASC", function (err, 
 	temperature1 = [];
 	temperature2 = [];
 	temperature3 = [];
+	people = 0;
+	data_time = new Date();
+	var data_index=0;
+	var data_value = 1000000000000;
 	for(var i in result){
 		labelsResult.push(result[i]["measur_date"].toString().slice(4,-21));
 		temperature1.push(result[i]["temperature1"]);
 		temperature2.push(result[i]["temperature2"]);
 		temperature3.push(result[i]["temperature3"]);
-		}
-     for(i in labelsResult){
+		 if(Math.abs(data_time-newDate(result[i]["measur_date"].toString().slice(4,-21)))<data_value){
+			 data_value = Math.abs(data_time-newDate(result[i]["measur_date"].toString().slice(4,-21)));
+			 data_index = i;
+		 };
+
+	}
+	console.log(result[i]["measur_date"]);
+	var temper = (result[data_index]["temperature1"]+result[data_index]["temperature2"]+result[data_index]["temperature3"])/3;
+	     for(i in labelsResult){
 	  labels=labels+'\''+labelsResult[i]+'\',';
   }
-  
-  
-  
-  fs.readFile('index.html', 'utf-8', function (err, data) {
+  con.query("select * from users", function (err, result2) {
+	  if (err) throw err;
+	  for(var j in result2){
+		  if(result2[j]["status_in_room"]==1){
+			  people++;
+		  }
+	  }
+	  fs.readFile('index.html', 'utf-8', function (err, data) {
         response.writeHead(200, { 'Content-Type': 'text/html' });
 		data=data.replace('XLABELSX', labels);
 		var datasets = '';
@@ -93,9 +165,15 @@ con.query("select * from measurements order by measur_date ASC", function (err, 
 		dataset = "{borderColor: 'rgba(0, 0, 255, 0.5)',backgroundColor: 'rgba(0, 0, 0, 0)',label: 'Temperature3',data: [XDATAX]}";
 		datasets = datasets+dataset.replace("XDATAX",temperature3.toString())+',';
 		data = data.replace('XDATASETSX', datasets);
+		data = data.replace('XPEOPLEX',people.toString());
+		data = data.replace('XTEMPERATUREX',temper.toString());
+		data = data.replace('XFIREX',result[data_index]["smoke_sensor"].toString());
         response.write(data);
         response.end();
     });
+  });
+  
+  
   });
  
 	
