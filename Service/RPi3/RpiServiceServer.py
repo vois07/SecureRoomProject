aad37b29fa2bb9681f10adfbb1aabea1286ec798
eaglx <tango_delta@outlook.com>
2018-12-30 17:25:01 +0100

    Update

--- Service/RPi3/RpiServiceServer.py
+++ Service/RPi3/RpiServiceServer.py
@@ -52,18 +52,33 @@ class SrvHandler(threading.Thread):
     def run(self):
         recvData = recv_until(self.clientSock, "%&", 102400) #102400 == 100KB
         if(recvData != False):
-            #temp1, temp2, temp3, smoke = recvData.split("%&")[0].split(" ")
-            print('[DATA RECIVE]\n', recvData)
-            # sqlTask = "INSERT INTO `measurements` (`measur_date`, `temperature1`, `temperature2`, `temperature3`, `smoke_sensor`) VALUES (%s, %s, %s, %s, %s)"
-            # curDB.execute(sqlTask, (strftime("%Y-%m-%d %H:%M:%S", gmtime()), temp1, temp2, temp3, smoke))
-            # connDB.commit()
+            timeMeasure = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            temp1 = 0
+            temp2 = 0
+            temp3 = 0
+            smoke = 0
+            dataLen = len(recvData.split("%&")[0].split(" "))
+            if dataLen == 1:
+                temp1 = temp2 = temp3 = recvData.split("%&")[0].split(" ")[0]
+            elif dataLen == 2:
+                temp1 = recvData.split("%&")[0].split(" ")[0]
+                temp2 = recvData.split("%&")[0].split(" ")[1]
+                temp3 = temp2
+            elif dataLen == 3:
+                temp1, temp2, temp3 = recvData.split("%&")[0].split(" ")
+            elif dataLen == 4:
+                temp1, temp2, temp3, smoke = recvData.split("%&")[0].split(" ")
+            print("[ ",  timeMeasure, temp1, temp2, temp3, smoke, " ]")
+            sqlTask = "INSERT INTO `measurements` (`measur_date`, `temperature1`, `temperature2`, `temperature3`, `smoke_sensor`) VALUES (%s, %s, %s, %s, %s)"
+            curDB.execute(sqlTask, (timeMeasure, temp1, temp2, temp3, smoke))
+            connDB.commit()
         #Close connection in both sides: client and server, no data send
         self.clientSock.shutdown(socket.SHUT_RDWR)
         self.clientSock.close()
 
 
 def main():
-  print("[ INFO ] RpiServiceServer v0.1")
+  print("[ INFO ] RpiServiceServer v0.2")
   print("[ INFO ] Server start at: ", strftime("%Y-%m-%d %H:%M:%S", gmtime()))
   #Get server IP
   ip_server = ni.ifaddresses(NET_DEVICE_INTERFACE)[ni.AF_INET][0]['addr']
@@ -78,7 +93,7 @@ def main():
   try:
     while True:
       clientSock, clientAddr = server.accept()
-      print("[ INFO ] Connection from: ", clientAddr)
+      #print("[ INFO ] Connection from: ", clientAddr)
       clientThread = SrvHandler(clientSock, clientAddr)
       clientThread.daemon = False #When kill main process, then it kill threads
       clientThread.start()
